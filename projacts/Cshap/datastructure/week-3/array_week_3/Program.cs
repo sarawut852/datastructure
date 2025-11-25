3687c5dd9a524f14100565db527988973a4b3c95
sarawut852 <sarawutx233@gmail.com>
2025-11-25 17:50:55 +0700

    Add files via upload

---
+++ projacts/Cshap/datastructure/week-3/array_week_3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks.Dataflow;
+
+class Program
+{
+    static void Main()
+    {
+        int[] scores = {10, 20, 30, 40, 50, 60, 70, 80, 90};
+        Console.WriteLine("รายการข้อมูลใน Array :");
+
+        //for loop
+        for (int i =0; i < scores.Length; i++)
+        {
+            Console.WriteLine($"index {i} = {scores[i]}");
+        }
+        //foreach
+        foreach(int x in  scores)
+        {
+            Console.WriteLine($"Value is :{x}");
+        }
+        int sum = 0;
+        for (int i =0; i < scores.Length; i++)
+        {
+            sum += scores[i];
+        }
+        Console.WriteLine($"Sum ={sum}"); //ผลรวม 
+        double avg = (double)sum / scores.Length;
+        Console.WriteLine($"Average : {avg}"); //ค่าเฉลี่ย
+        //for max, min
+        int max = scores[0];
+        int min = scores[0];
+        for (int i = 0; i < scores.Length; i++)
+        {
+            // Max value check 
+            if(scores[i] > max)
+            {
+                max = scores[i];
+            }
+            //Min value check
+            if(scores[i] < min)
+            {
+                min = scores[i];
+            }
+        }
+        Console.WriteLine($"max ={max}");
+        Console.WriteLine($"min ={min}");
+    }
+}
