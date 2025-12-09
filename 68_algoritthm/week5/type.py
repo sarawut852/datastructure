60b69eff77ffb00d336b4882a318fc978ec4fe27
sarawut852 <sarawutx233@gmail.com>
2025-12-09 11:55:17 +0700

    Add files via upload

---
+++ 68_algoritthm/week5/type.py
@@ -0,0 +1,32 @@
+FullName = ("Sarawut")
+StudenID = 684245015
+Gpax = 3.25
+Age = 18
+
+
+#print(type(x))
+#print(type(y))
+#print(type(z))
+
+def checkType(data):
+    #print(type(data))
+    return type(data)
+
+      
+      
+if(type(FullName) == str):
+    print("Sarawut is str")
+else:
+    print("Sarawut is str")
+if(type(StudenID) == complex):
+    print("684245015 is complex")
+else:
+    print("684245015 is not complex")
+if(type(Gpax) == float):
+    print("3.25 is float")
+else:
+    print("3.25 is not float")
+if(type(Age) == int):
+    print("18 is int")
+else:
+    print("18 is not int")
